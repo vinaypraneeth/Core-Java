9f6451c1ad623c74ad16540aed1cfcf4c4cfb156
vinaypraneeth <142254904+vinaypraneeth@users.noreply.github.com>
2024-03-02 20:39:52 +0530

    Add files via upload

---
+++ Logical_Operator/Logical_AND_/logicaloperator.java
@@ -0,0 +1,10 @@
+class logic
+{
+    public static void main(String [] args)
+    {
+        String color ="red";
+        double price=900d;
+        boolean res=(color=="red" && price==900d);
+        System.out.println(res);
+    }
+}
---
+++ Logical_Operator/Logical_OR/logical_or.java
@@ -0,0 +1,12 @@
+public class logical_or
+ 
+{
+    public static void main(String[] args) 
+    {
+        String color="black";
+        double price=900d;
+        System.out.println(color=="red" || price<=900d);
+        
+    }
+    
+}
